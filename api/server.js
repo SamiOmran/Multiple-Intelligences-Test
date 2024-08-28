b4bda62ab1aa06dcd13af6c7be4571a850f01173
Sami Imran <samiimran2016@gmail.com>
2024-08-28 23:18:36 +0300

    fix server side

--- api/server.js
+++ api/server.js
@@ -1,4 +1,4 @@
-const Result = require('./models/resultSchema');
+const Result = require('../models/resultSchema');
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -6,10 +6,15 @@ const cors = require('cors');
 const app = express();
 const path = require('path');
 
+let dirPath = __dirname;
+dirPath = dirPath.substring(0, 39);
+// console.log(dirPath);
+
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
-app.use('/assets', express.static(__dirname + '/assets'));
+app.use('/static', express.static(path.join(dirPath, '/static')));
+app.use('/assets', express.static(path.join(dirPath, '/assets')));
 
 // Endpoint to store results
 app.post('/api/results', async (req, res) => {
@@ -26,7 +31,7 @@ app.post('/api/results', async (req, res) => {
 
 // Endpoint to retrieve results by teacher name
 app.get('/api/results/', async (req, res) => {
-	const fields = { '_id': 0, 'teacherName': 1, 'results': 1 };
+	const fields = { _id: 0, teacherName: 1, results: 1 };
 
 	try {
 		const teacherResults = await Result.find().select(fields);
@@ -36,10 +41,16 @@ app.get('/api/results/', async (req, res) => {
 	}
 });
 
-app.get('/results', (req, res) => {	
-	res.sendFile(path.join(__dirname, './static/results.html'));
-})
+app.get('/results', (req, res) => {
+	res.sendFile(path.join(dirPath, '/static/results.html'));
+});
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(dirPath, '/static/index.html'));
+});
 
 // Start the server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () =>
+	console.log(`Server is running on: https://localhost:${PORT}/`)
+);
