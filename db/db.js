fcb225766813bcf78a7cba098981e399435f2b47
Sami Imran <samiimran2016@gmail.com>
2024-08-29 00:54:20 +0300

    use mongo atlas

--- db/db.js
+++ db/db.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
+require('dotenv').config()
+
+const uri = process.env.MONGO_URI
 
 // Connect to MongoDB
 mongoose
-	.connect('mongodb://localhost:27017/intelligenceTest', {
+	// .connect('mongodb://localhost:27017/intelligenceTest', {
+	.connect(uri, {
 		// useNewUrlParser: true,
 		// useUnifiedTopology: true,
 	})
